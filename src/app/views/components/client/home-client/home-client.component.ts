4d80753baf77c04fead37f60a762f5235bc724e4
PILLAJO MONTACHANA KEVIN HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2021-10-29 09:38:58 -0500

    Update home-client.component.ts

--- src/app/views/components/client/home-client/home-client.component.ts
+++ src/app/views/components/client/home-client/home-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/shared/models/user-model';
 
 @Component({
   selector: 'app-home-client',
@@ -9,17 +10,21 @@ export class HomeClientComponent implements OnInit {
 
   lectura:boolean = true;
 
-  /* crear un formulario y que el formlario tome ñlos valores de localStorage de userData pathValue */
+  /* crear un formulario y que el formlario tome los valores de localStorage de userData pathValue */
 
   /* crear un formulario html con la directivas *ngIf */
 
-
+  userData: UserModel;
+  token:string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.userData = JSON.parse(localStorage.getItem('userData'));
+    this.token = JSON.parse(localStorage.getItem('accessToken'));
   }
 
   /* crear una funcion que hacer update  */
 
 }
+
